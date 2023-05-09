e51867fc96a55fa64b30410c44a0e4378fa4bbdc
alessiogentilezza <alessio.gentilezza@gmail.com>
2023-05-09 14:30:43 +0200

    primo commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>laravel-primi-passi</title>
+</head>
+<style>
+    body {
+        font-family: 'Nunito', sans-serif;
+    }
+</style>
+
+<body>
+    <p>Hello World</p>
+</body>
+
+</html>
